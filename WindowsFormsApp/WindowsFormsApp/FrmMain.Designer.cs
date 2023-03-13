262e405e422e16b072af2effb12d9aa8c0a2a777
Far9554 <marioleoncarrizo@gmail.com>
2023-03-13 09:06:46 +0100

    Ex5

--- WindowsFormsApp/WindowsFormsApp/FrmMain.Designer.cs
+++ WindowsFormsApp/WindowsFormsApp/FrmMain.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 38);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Viatge";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -47,7 +47,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 38);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Menjar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // FrmMain
